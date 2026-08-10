cf24f0a0a91dc6721bd93b6af82bd7cdab287e42
sagyzdop <sagyzdop@gmail.com>
2026-08-10 13:40:41 +0500

    chore: rebrand page title from MerikSirat to NU Image

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,7 +22,7 @@ export const Route = createRootRouteWithContext<{
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'MerikSirat',
+        title: 'NU Image',
       },
     ],
     links: [
